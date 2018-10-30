3dd0f923b864330b92200c1d2f9427409335eff4
Coffee Warrior <lionlikelucas@gmail.com>
2018-10-30 15:42:53 -0400

    adding turning

--- src/stateful/Canvas.js
+++ src/stateful/Canvas.js
@@ -6,31 +6,37 @@ class Canvas extends Component{
 
 
         this.state = {
-            shipColor: "#66CD00",
             backgroundColor: "#000",
             shipPosition: {
-                x: 750,
-                y: 350
+                x: 0,
+                y: 0
             },
             keyMap : {
                 w: false,
                 s: false,
                 a: false,
                 d: false
-            }
+            },
+            shipIMG : null,
+            shipLoaded: false,
+            planeAngle : 0
         }
     }
 
+    
+    
+
     //draws the ship. Used as a utility function by other this.move()
-    drawShip(color, lineWidth){
-        //need to move image outside of function, so it doesnt reload every time!
+    drawShip(){
         const ctx = this.refs.canvas.getContext('2d');
-        
-        var img = new Image(50,50);
-        img.onload = () => {
-            ctx.drawImage(img, this.state.shipPosition.x, this.state.shipPosition.y);
+        if(this.state.shipLoaded){
+
+            // ctx.rotate(20 * Math.PI / 180);
+            //commented will cause spinning, need to adjust the canvas angle outside of 
+            ctx.drawImage(this.state.shipIMG, this.state.shipPosition.x, this.state.shipPosition.y)
         }
-        img.src = ship;
+
+        
     }
 
     //updates the state to reflect movement by wsad and redraws the ship.
@@ -39,34 +45,33 @@ class Canvas extends Component{
         let currentPosition = {...prevState.shipPosition}
 
 
-            if(this.state.keyMap.w){
-                currentPosition.y -= 5;
-                this.setState({shipPosition: currentPosition})
-            }
+        if(this.state.keyMap.w){
+            currentPosition.y -= 5;
+            this.setState({shipPosition: currentPosition})
+        }
 
-            if(this.state.keyMap.s){
-                currentPosition.y += 5;
-                this.setState({shipPosition: currentPosition})
-            }
+        if(this.state.keyMap.s){
+            currentPosition.y += 5;
+            this.setState({shipPosition: currentPosition})
+        }
 
-            if(this.state.keyMap.a){
-                currentPosition.x -= 5;
-                this.setState({shipPosition: currentPosition})
-            }
+        if(this.state.keyMap.a){
+            currentPosition.x -= 5;
+            this.setState({shipPosition: currentPosition})
+        }
 
-            if(this.state.keyMap.d){
-                currentPosition.x += 5;
-                this.setState({shipPosition: currentPosition})
-            }
-                
-         
-        this.drawShip(this.state.shipColor, 2);
+        if(this.state.keyMap.d){
+            currentPosition.x += 5;
+            this.setState({shipPosition: currentPosition})
+        }
+        
+        this.drawShip();
     }
 
     //function called by setInterval, responsible for redraws of the canvas.
     drawCanvas = () => {
         const ctx = this.refs.canvas.getContext('2d');
-        ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+        ctx.clearRect(-this.refs.canvas.width/2, this.refs.canvas.height/2, this.refs.canvas.width/2, this.refs.canvas.height/2);
 
         this.move();
         
@@ -74,13 +79,13 @@ class Canvas extends Component{
 
     //updates keymap in state, will set boolean values to true or false on keydown and keyup 
     adjustKeyMap(event){
+        let directionChange = true;
         if(event.type === "keydown"){
             const keyMap = {...this.state.keyMap}
             switch(event.key){
                 case "w":
                     keyMap.w = true;
                     break;
-    
                 case "s":
                     keyMap.s = true;
                     break;
@@ -93,6 +98,8 @@ class Canvas extends Component{
                     keyMap.d = true;
                     break;
                 default:
+                    directionChange = false;
+                    break;
             }
             this.setState({keyMap: keyMap})
 
@@ -115,15 +122,28 @@ class Canvas extends Component{
                     keyMap.d = false;
                     break;
                 default:
+                    directionChange = false;
             }
+
             this.setState({keyMap: keyMap})
         
         }
     }
 
-    componentDidMount(){
-        setInterval(this.drawCanvas, 25)
 
+    setShip = () => {
+        var img = new Image(50,50);
+        img.onload = () => {
+            this.setState({shipIMG: img, shipLoaded: true})
+        }
+        img.src = ship;
+    }
+
+    componentDidMount(){
+        const ctx = this.refs.canvas.getContext('2d');
+        ctx.translate(750, 325);
+        setInterval(this.drawCanvas, 25)
+        this.setShip();
         window.addEventListener("keydown", (e) => {this.adjustKeyMap(e)})
         window.addEventListener("keyup", (e) => {this.adjustKeyMap(e)})
     }
@@ -131,8 +151,7 @@ class Canvas extends Component{
     render(){
         return(
             <div>
-            <canvas ref="canvas" width = "1500px" height = "700px" style={{backgroundColor: `${this.state.backgroundColor}`}}></canvas>
-            <img src={ship}></img>
+            <canvas ref="canvas" height = "750" width = "1500"  style={{backgroundColor: `${this.state.backgroundColor}`}}></canvas>
             </div>
         )
     }
