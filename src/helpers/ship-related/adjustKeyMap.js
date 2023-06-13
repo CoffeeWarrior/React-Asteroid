cdb5bc76d596bb6d1eee979742cab44285128cd8
Lucas Goldman <Lucas.Goldman99@gmail.com>
2023-06-13 12:01:03 -0400

    fixed old cursed mistake of not adding arrow keys to move options

--- src/helpers/ship-related/adjustKeyMap.js
+++ src/helpers/ship-related/adjustKeyMap.js
@@ -1,49 +1,56 @@
-//updates keymap in state, will set values to true or false on keydown and keyup 
-const adjustKeyMap = (event, keyMapInput) =>{
-	if(event.type === "keydown"){
-		const keyMap = {...keyMapInput}
-		switch(event.key){
-		case "w":
-			keyMap.w = true
-			break
-		case "s":
-			keyMap.s = true
-			break
-        
-		case "a":
-			keyMap.a = true
-			break
-                
-		case "d":
-			keyMap.d = true
-			break
-		default:
-			break
-		}
-		return keyMap
+//updates keymap in state, will set values to true or false on keydown and keyup
+const adjustKeyMap = (event, keyMapInput) => {
+  if (event.type === "keydown") {
+    const keyMap = { ...keyMapInput };
+    switch (event.keyCode) {
+      case 87:
+      case 38:
+        keyMap.w = true;
+        break;
+      case 83:
+      case 40:
+        keyMap.s = true;
+        break;
 
-	}else if(event.type === "keyup"){        
-		const keyMap = {...keyMapInput}
-		switch(event.key){
-		case "w":
-			keyMap.w = false
-			break
+      case 65:
+      case 37:
+        keyMap.a = true;
+        break;
 
-		case "s":
-			keyMap.s = false
-			break
-        
-		case "a":
-			keyMap.a = false
-			break
-                
-		case "d":
-			keyMap.d = false
-			break
-		default:
-		}
+      case 68:
+      case 39:
+        keyMap.d = true;
+        break;
+      default:
+        break;
+    }
+    return keyMap;
+  } else if (event.type === "keyup") {
+    const keyMap = { ...keyMapInput };
+    switch (event.keyCode) {
+      case 87:
+      case 38:
+        keyMap.w = false;
+        break;
 
-		return keyMap
-	}
-}
-export default adjustKeyMap
+      case 83:
+      case 40:
+        keyMap.s = false;
+        break;
+
+      case 65:
+      case 37:
+        keyMap.a = false;
+        break;
+
+      case 68:
+      case 39:
+        keyMap.d = false;
+        break;
+      default:
+    }
+
+    return keyMap;
+  }
+};
+export default adjustKeyMap;
